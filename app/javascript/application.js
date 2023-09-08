46967f449cad185f17fa1178a9d715147802ad82
Victory <victory.someth@gmail.com>
2023-09-09 00:31:30 +0800

    Clean up layout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "controllers"
+
+//= require jquery
+//= require jquery_ujs
+//= require popper
+//= require bootstrap
